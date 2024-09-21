b1bfa541961b76a7f601d19f702c4a75f80ba57a
Digvijay Singh <digvijays386@gmail.com>
2024-09-21 14:30:02 +0100

    added log::info

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -169,16 +169,23 @@ class AuthController extends Controller
      */
     public function login(Request $request)
     {
+
+        Log::info('request from mobile.', $request);
+
         $description = "Verification code ";
         $otp = rand(1111, 9999);
         $user = User::where('email', $request->email)->first();
 
+        Log::info('User found failed to login.', ['id' => $user->id]);
         if ($user) {
             if (Hash::Check($request->password, $user->password)) {
                 $interest = UserIntrest_pivot::where('user_id', $user->id)->get();
-                dd('user found');
+                Log::info('User {id} failed to login.', ['id' => $user->id]);
+
                 $token = $user->createToken('LaravelAuthApp')->accessToken;
-                dd('token created');
+
+                Log::info('failed to login access token.', ['id' => $request]);
+
                 if (count($interest) > 0)
                     $interestStatus = true;
                 else
@@ -205,6 +212,8 @@ class AuthController extends Controller
                     'error' => False
 
                 ]);
+                Log::info('not reached to response.', $user);
+
             } else {
                 return response()->json([
                     "message" => "Password Missmatch",
